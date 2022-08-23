3b3b3ab3865ee24ecc076238595656ec806bbaf3
davidkube <david.dk.kube@gmail.com>
2022-08-23 18:47:03 +0200

    build.sh updated

--- packages/understat/mine/build.sh
+++ packages/understat/mine/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 set -e
 
-virtualenv virtualenv
-source virtualenv/bin/activate
-pip install -r requirements.txt
-deactivate
+virtualenv --without-pip virtualenv
+pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
