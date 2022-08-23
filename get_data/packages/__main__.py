d7521db4ef726a53d829413f09a62fa3a9158985
davidkube <david.dk.kube@gmail.com>
2022-08-23 14:01:00 +0200

    first commit

---
+++ get_data/packages/__main__.py
@@ -0,0 +1,17 @@
+import asyncio
+import json
+import aiohttp
+
+from understat.understat import Understat
+
+async def main(args):
+      async with aiohttp.ClientSession() as session:
+            understat = Understat(session)
+            test = await understat.get_league_matches('EPL',2021)
+            print(json.dumps(test))
+            
+      
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main([]))
