d111f1f6b191ad649a13a4bef128c5ccaac2e806
davidkube <david.dk.kube@gmail.com>
2022-08-24 17:37:19 +0200

    Parameterized function - now can be called for different leagues and seasons

--- packages/understat/mine/__main__.py
+++ packages/understat/mine/__main__.py
@@ -1,19 +1,55 @@
 import asyncio
-import json
 import aiohttp
+from datetime import datetime
+import pymongo
+from pymongo.write_concern import WriteConcern
+import sys
 
 from understat import Understat
+
+
+LEAGUES = {
+      "epl": "EPL",
+      "la_liga": "La_liga",
+      "bundesliga": "Bundesliga",
+      "serie_a": "Serie_A",
+      "ligue_1": "Ligue_1",
+      "rfpl": "RFPL"
+}
+
+START_SEASON = 2014
+
+def rename_id(dict):
+      dict['_id'] = dict.pop('id')
+      return dict
 
 async def main(args):
       async with aiohttp.ClientSession() as session:
             understat = Understat(session)
-            test = await understat.get_league_matches('EPL',2021)
-            print(json.dumps(test))
+            list_of_matches = await understat.get_league_matches(sys.argv[1],sys.argv[2]) 
+
+            reindexed = [rename_id(match) for match  in list_of_matches]
+            args[0].delete_many( { '_id' : { '$in': [match['_id'] for match in reindexed] } } );
+            args[0].insert_many(reindexed)
             
       
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main([]))
+      LEAGUE = sys.argv[1]
+      SEASON = sys.argv[2]
 
-    
+      cont = True
+      if LEAGUE not in LEAGUES.values():
+            print('Invalid League')
+            cont = False
+      currentYear = datetime.now().year
+      if int(SEASON) not in range(START_SEASON,currentYear+1):
+            print('Invalid Season : ' + str(SEASON) + ' not in (' + ', '.join([str(x) for x in range(START_SEASON,currentYear+1)]) + ')')
+            cont = False
+      if cont:
+            print( LEAGUE + ' : ' + str(SEASON))
+            CLIENT = pymongo.MongoClient("mongodb+srv://doadmin:u807mD45kl192cNP@shorthub-9b85bbdd.mongo.ondigitalocean.com/understat?tls=true&authSource=admin&replicaSet=shorthub")
+            DB = CLIENT["understat"]
+            COLL = DB["matches"]
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(main([COLL]))
